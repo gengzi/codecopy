a9d8f2a7803f246660634cb55f24b20bf82f0259
gengzi <1164014750@qq.com>
2020-05-13 23:35:16 +0800

    init

---
+++ src/main/java/fun/gengzi/codecopy/CodecopyApplication.java
@@ -0,0 +1,13 @@
+package fun.gengzi.codecopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecopyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/gengzi/codecopy/CodecopyApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.gengzi.codecopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
