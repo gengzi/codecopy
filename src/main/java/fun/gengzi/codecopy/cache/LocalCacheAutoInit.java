c0559e3709a2d94f91d9bdfc95044ca0e83c4109
gengzi <1164014750@qq.com>
2020-06-14 16:19:36 +0800

    [update] test

--- src/main/java/fun/gengzi/codecopy/cache/LocalCacheAutoInit.java
+++ src/main/java/fun/gengzi/codecopy/cache/LocalCacheAutoInit.java
@@ -37,34 +37,38 @@ public class LocalCacheAutoInit implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        logger.info("系统本地缓存 SysLocalCacheinit start");
-        sysPermissionService.getAllInfoByPid(310512);
-        sysPermissionService.getInfoByType("F");
-        logger.info("系统本地缓存 SysLocalCacheinit end");
+
+        
 
 
-        logger.info("验证缓存是否存在1");
-        List<SysPermission> allInfoByPid = sysPermissionService.getAllInfoByPid(310512);
-        allInfoByPid.forEach(
-                sysPermission -> {
-                    System.out.println(sysPermission.toString());
-                }
-        );
-        logger.info("验证缓存是否存在2");
-        // 每次都会执行sql ，把缓存放进去
-        List<SysPermission> f = sysPermissionService.getInfoByType("F");
-        f.forEach(sysPermission -> System.out.println(sysPermission.toString()));
-
-        logger.info("清除缓存");
-        sysPermissionService.clearTypeCache(310512);
-
-        logger.info("验证缓存是否存在3");
-        List<SysPermission> allInfoByPid2 = sysPermissionService.getAllInfoByPid(310512);
-        allInfoByPid2.forEach(
-                sysPermission -> {
-                    System.out.println(sysPermission.toString());
-                }
-        );
+//        logger.info("系统本地缓存 SysLocalCacheinit start");
+//        sysPermissionService.getAllInfoByPid(310512);
+//        sysPermissionService.getInfoByType("F");
+//        logger.info("系统本地缓存 SysLocalCacheinit end");
+//
+//
+//        logger.info("验证缓存是否存在1");
+//        List<SysPermission> allInfoByPid = sysPermissionService.getAllInfoByPid(310512);
+//        allInfoByPid.forEach(
+//                sysPermission -> {
+//                    System.out.println(sysPermission.toString());
+//                }
+//        );
+//        logger.info("验证缓存是否存在2");
+//        // 每次都会执行sql ，把缓存放进去
+//        List<SysPermission> f = sysPermissionService.getInfoByType("F");
+//        f.forEach(sysPermission -> System.out.println(sysPermission.toString()));
+//
+//        logger.info("清除缓存");
+//        sysPermissionService.clearTypeCache(310512);
+//
+//        logger.info("验证缓存是否存在3");
+//        List<SysPermission> allInfoByPid2 = sysPermissionService.getAllInfoByPid(310512);
+//        allInfoByPid2.forEach(
+//                sysPermission -> {
+//                    System.out.println(sysPermission.toString());
+//                }
+//        );
 
 
 //        logger.info("系统本地缓存 SysLocalCacheinit end");
