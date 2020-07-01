7b48dd98257a1b22486d13625aacd3725bbf8f6e
gengzi <1164014750@qq.com>
2020-07-01 15:34:29 +0800

    [update] version1.8 增加 application active 标签可以配置多个生效的配置文件

--- src/main/java/fun/gengzi/codecopy/config/ConfigProperties.java
+++ src/main/java/fun/gengzi/codecopy/config/ConfigProperties.java
@@ -5,20 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
- *  多个 yml 配置加载类
- *  参考：https://www.cnblogs.com/ligh-test/p/10752072.html
+ * 多个 yml 配置加载类
+ * 参考：https://www.cnblogs.com/ligh-test/p/10752072.html
+ *
  * @author gengzi
  * @date 2020年6月29日17:44:50
  */
-@Configuration
+//@Configuration
 public class ConfigProperties {
 
     private static Logger logger = LoggerFactory.getLogger(ConfigProperties.class);
@@ -31,11 +29,6 @@ public class ConfigProperties {
         logger.info("-- loading yml start --");
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
         YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
-//        ArrayList<ClassPathResource> classPathResources = new ArrayList<ClassPathResource>();
-//        Arrays.stream(names.split(",")).forEach(name ->{
-//            classPathResources.add(new ClassPathResource(name));
-//        });
-//        classPathResources.toArray();
         yaml.setResources(new ClassPathResource("redis.yml"));
         configurer.setProperties(yaml.getObject());
         return configurer;
