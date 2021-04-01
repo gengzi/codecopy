4a413fc83323579b88c075d08cad202bf65f3373
gengzi <1164014750@qq.com>
2021-04-01 22:43:52 +0800

    [update] version 1.13 自定义类加载器

--- src/main/java/fun/gengzi/codecopy/business/classloader/hotdeployment/HotDeploymentClassLoaderV2.java
+++ src/main/java/fun/gengzi/codecopy/business/classloader/hotdeployment/HotDeploymentClassLoaderV2.java
@@ -23,7 +23,16 @@ public class HotDeploymentClassLoaderV2 extends URLClassLoader {
     }
 
     /**
-     * 建议使用findclass 是不想在破坏双亲委派模型的情况下，才这样
+     *
+     * 如果所加载得类库，不在当前用户路径（classpath）下，才会执行重写的 findclass 加载
+     *
+     * 建议重写findclass 是尽量遵循双亲委派模型的情况下，才这样使用
+     * 也就是你在当前应用下写的代码，使用自定义类加载器，该类加载器只重写了findclass方法，这些代码类不会被自定义类加载器加载，都会被appclassloader加载。
+     * 或者设置自定义类加载器的父加载器是null 或者 ext 来避免父类加载器是 appclassClassloader，这样bootstrap 和 ext 类加载器，因无法加载你写的类，就会执行findclass逻辑
+     *
+     *
+     * 如果你想设置用户路径下的某些类加载，使用自定义类加载器，最好重写loaderclass方法，更加自由
+     *
      * @param name
      * @return
      * @throws ClassNotFoundException
