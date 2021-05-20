d5514c8519eba85ff0d8e1ac42fa55cf718c7c94
gengzi <1164014750@qq.com>
2021-05-20 23:30:24 +0800

    [update] version 1.13 tomcat 返回静态页面

--- src/main/java/fun/gengzi/codecopy/business/tomcat/BootStrap.java
+++ src/main/java/fun/gengzi/codecopy/business/tomcat/BootStrap.java
@@ -8,24 +8,23 @@ import java.net.Socket;
 
 
 /**
- *  <h1>mytomcat 启动类</h1>
- *
- *  1.0 版本，支持接收HTTP请求，并成功响应 hello mytomcat
- *  2.0 版本，封装Request 和 Response 对象，并响应静态资源
- *
- *
- *
- *
- *
- *
+ * <h1>mytomcat 启动类</h1>
+ * <p>
+ * 1.0 版本，支持接收HTTP请求，并成功响应 hello mytomcat
+ * 2.0 版本，封装Request 和 Response 对象，并响应静态资源
  *
  * @author gengzi
  * @date 2021年5月19日15:09:38
  */
 public class BootStrap {
 
-
+    // 监听端口
     public static final int PORT = 8080;
+    // 静态页面后缀
+    public static final String STATIC_HTML_TYPE = ".html";
+
+    // get请求方式
+    public static final String GET_HTTP = "GET";
 
 
     /**
@@ -44,8 +43,11 @@ public class BootStrap {
 
     }
 
+    /**
+     * 版本1 支持接收HTTP请求，并成功响应 hello mytomcat
+     */
     @SneakyThrows
-    public static void version1(){
+    public static void version1() {
         ServerSocket serverSocket = new ServerSocket(PORT);
         while (true) {
             // 只有接收到请求后，该方法才会返回
@@ -69,18 +71,24 @@ public class BootStrap {
     }
 
 
-
+    /**
+     * 版本2 封装Request 和 Response 对象，并响应静态资源
+     */
     @SneakyThrows
-    public static void version2(){
+    public static void version2() {
         ServerSocket serverSocket = new ServerSocket(PORT);
         while (true) {
             // 只有接收到请求后，该方法才会返回
             Socket socket = serverSocket.accept();
             // 获取数据
             Request request = new Request(socket);
-
+            // 进行响应
             Response response = new Response(socket);
-
+            if (request.getUrl().endsWith(STATIC_HTML_TYPE) &&  GET_HTTP.equalsIgnoreCase(request.getHttpType())) {
+                response.jumpStaticResource(request.getUrl());
+            }else {
+                response.outPut("找不到此页面");
+            }
             socket.close();
         }
     }
