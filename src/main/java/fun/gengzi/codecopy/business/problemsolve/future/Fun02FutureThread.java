49d6b582a35e46dcc9998ae4efdce920f2903fa1
gengzi <1164014750@qq.com>
2021-08-26 17:19:18 +0800

    [update] version 1.14 测试futrue 是否能中断线程

--- src/main/java/fun/gengzi/codecopy/business/problemsolve/future/Fun02FutureThread.java
+++ src/main/java/fun/gengzi/codecopy/business/problemsolve/future/Fun02FutureThread.java
@@ -15,33 +15,37 @@ public class Fun02FutureThread {
         // 不能获取返回结果
         // threadPoolExecutor.execute();
 
-        // 可以得到一个返回结果
-        Future<?> future = threadPoolExecutor.submit(() -> {
-            try {
-                for (int i = 0; i < 1000; i++) {
-                    System.out.println("执行" + i);
-                    Thread.sleep(1000);
+        for (int j= 0; j< 10; j++) {
+
+            // 可以得到一个返回结果
+            Future<?> future = threadPoolExecutor.submit(() -> {
+                try {
+                    for (int i = 0; i < 1000; i++) {
+                        System.out.println("执行" + i);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+            });
+            // submit 可以传一个string ，并响应回来
+
+            try {
+                future.get(2, TimeUnit.SECONDS);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-        });
-        // submit 可以传一个string ，并响应回来
-
-        try {
-            future.get(2, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            // TODO 必须加return；
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-            boolean cancel = future.cancel(true);
-            System.out.println(cancel);
-        } finally {
+                // TODO 必须加return；
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+                boolean cancel = future.cancel(true);
+                System.out.println(cancel);
+            } finally {
 //            threadPoolExecutor.shutdownNow();
+            }
         }
+
 
         Thread.sleep(50000000);
 
