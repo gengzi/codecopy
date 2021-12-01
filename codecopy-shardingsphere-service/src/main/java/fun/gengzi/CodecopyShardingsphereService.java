50b5679b3b8fdb6def718a50002c7aa36d4ee443
gengzi <1164014750@qq.com>
2021-12-01 16:56:02 +0800

    [version1.0] 内容：接入shardingsphere 5.0 版本依赖，基础配置并初始化

--- codecopy-shardingsphere-service/src/main/java/fun/gengzi/CodecopyShardingsphereService.java
+++ codecopy-shardingsphere-service/src/main/java/fun/gengzi/CodecopyShardingsphereService.java
@@ -1,12 +1,26 @@
 package fun.gengzi;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import fun.gengzi.config.TransactionConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 
-@SpringBootApplication
+/**
+ * <H1>Mysql分库分表实践</H1>
+ *
+ * @author gengzi
+ * @date 2021年12月1日16:48:47
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class, JtaAutoConfiguration.class})
+// 忽略自动配置的类，排除 数据源自动配置，数据量连接池的自动配置
 @EnableEurekaClient
+// 导入分布式事务配置
+@Import(TransactionConfiguration.class)
 public class CodecopyShardingsphereService {
     public static void main(String[] args) {
         SpringApplication.run(CodecopyShardingsphereService.class, args);
---
+++ codecopy-shardingsphere-service/src/main/java/fun/gengzi/config/TransactionConfiguration.java
@@ -0,0 +1,61 @@
+package fun.gengzi.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.transaction.annotation.ShardingSphereTransactionType;
+import org.apache.shardingsphere.transaction.core.TransactionType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+
+/**
+ * <h1>分布式事务配置</h1>
+ *
+ * @author gengzi
+ * @date 2021年12月1日16:50:44
+ */
+// 声明配置类
+@Configuration
+// 启用事务管理器
+@EnableTransactionManagement
+@Slf4j
+public class TransactionConfiguration {
+
+    /**
+     * 初始化事务管理器
+     *
+     * @param dataSource 数据源
+     * @return 事务管理器
+     */
+    @Bean
+    public PlatformTransactionManager txManager(final DataSource dataSource) {
+        log.info("初始化txManager");
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+//    @Bean
+//    public JdbcTemplate jdbcTemplate(final DataSource dataSource) {
+//        return new JdbcTemplate(dataSource);
+//    }
+//
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @Transactional
+//    @ShardingSphereTransactionType(TransactionType.XA)  // 支持TransactionType.LOCAL, TransactionType.XA, TransactionType.BASE
+//    public void insert() {
+//        jdbcTemplate.execute("INSERT INTO t_order (user_id, status) VALUES (?, ?)", (PreparedStatementCallback<Object>) ps -> {
+//            ps.setObject(1, "1");
+//            ps.setObject(2, "init");
+//            ps.executeUpdate();
+//            return ps;
+//        });
+//    }
+}
